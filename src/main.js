ad69d817646391f9b6b86988d83d7e0fa04165d0
Patrick Hamod <pfhamod@gmail.com>
2018-12-11 18:25:06 -0800

    Initial commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import vgl from 'vue-golden-layout'
+Vue.use(vgl);
+import 'golden-layout/src/css/goldenlayout-dark-theme.css'
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div>
+	<button class="reset">Reset localStorage</button>
+	<golden-layout class="hscreen">
+		<div slot="stackCtr" class="test-template">
+			Added item
+			<button>Remove</button>
+			Hey patrick
+		</div>
+		<template slot="bottom">
+			Bottom
+		</template>
+		<gl-col :closable="false">
+			<gl-router>
+				<gl-route name="r-a" />
+				<template slot="route">
+				</template>
+			</gl-router>
+			<gl-row :closable="false">
+				<gl-component title="compA" class="test-component">
+					<h1>CompA</h1>
+					<button>Toggle</button>
+					<button>Add</button>
+					<p>
+            test component
+					</p>
+				</gl-component>
+				<gl-dstack ref="myStack" dstack-id="dynamics">
+					<gl-component title="Dstack demonstration" :closable="false" :reorder-enabled="false">
+						This element is just present to test the ability of the d-stack if this stack happens to be popped out.
+					</gl-component>
+				</gl-dstack>
+			</gl-row>
+			<gl-component template="bottom"  />
+		</gl-col>
+	</golden-layout>
+</div>
 </template>
